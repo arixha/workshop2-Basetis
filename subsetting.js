6cbba9ff64db9181e52028a122c6e8de68db3ae9
Sara Chodosh <sechodosh@gmail.com>
2021-04-18 11:26:36 -0400

    added apple's code

---
+++ subsetting.js
@@ -0,0 +1,101 @@
+const nodes = [{"n":{"type":"node","id":"26181215","labels":["Topic"],"properties":{"last_sentiment_score":9.39,"last_trend_prob_var":0.08,"num_pubs":3195,"origin":"E","mesh_id":"D017497","num_cts":0,"description":"A chronic suppurative and cicatricial disease of the apocrine glands occurring chiefly in the axillae in women and in the groin and anal regions in men. It is characterized by poral occlusion with secondary bacterial infection, evolving into abscesses which eventually rupture. As the disease becomes chronic, ulcers appear, sinus tracts enlarge, fistulas develop, and fibrosis and scarring become evident.","created_at":1617840178,"mesh_active":true,"tracked":true,"last_trend_prob":48.27,"uri":"http://id.nlm.nih.gov/mesh/D017497","mesh_term":"Hidradenitis Suppurativa","updated_at":1617972319,"supplementary_concept":false,"term":"Hidradenitis Suppurativa","topic_id":"05407cf9-1436-4261-a776-da361799d10d"}}},
+{"n":{"type":"node","id":"26190108","labels":["Topic"],"properties":{"mesh_term":"Hidradenitis suppurativa, familial","updated_at":1617840183,"supplementary_concept":true,"num_pubs":1,"origin":"E","num_cts":0,"mesh_id":"C538118","created_at":1617840178,"mesh_active":true,"term":"Hidradenitis suppurativa, familial","topic_id":"23e68998-2455-4fb4-b1ae-e80995b16ee9","uri":"http://id.nlm.nih.gov/mesh/C538118"}}},
+{"n":{"type":"node","id":"55238081","labels":["Product"],"properties":{"effective_time":1570060800,"product_type":"HUMAN PRESCRIPTION DRUG","updated_at":1618149996,"generic_name":"ADALIMUMAB","product_id":"c49f4803-8c12-4262-b780-6b097e163fb1","created_at":1618149996,"brand_name":"Humira","manufacturer_name":"A-S Medication Solutions","version":"1"}}},
+{"n":{"type":"node","id":"55233138","labels":["Product"],"properties":{"effective_time":1614124800,"product_type":"HUMAN PRESCRIPTION DRUG","updated_at":1618149620,"generic_name":"ADALIMUMAB","product_id":"874057c0-a437-4a08-9105-38c1b2f3307b","created_at":1618149620,"brand_name":"Humira","manufacturer_name":"AbbVie Inc.","found_in":"manufacturer_name","version":"2135"}}},
+{"n":{"type":"node","id":"55290087","labels":["Tweet"],"properties":{"updated_at":1618152686,"tweet_id":"1380981439697666055","created_at":1618152686}}},
+{"n":{"type":"node","id":"55125517","labels":["Tweet"],"properties":{"updated_at":1618065988,"tweet_id":"1380836558933262336","created_at":1618065988}}},
+{"n":{"type":"node","id":"55290084","labels":["Tweet"],"properties":{"date":1604805831,"sentiment":0.0,"updated_at":1618152686,"favourites":9,"hashtags":["HumanRights","PatientRights","HidradenitisSuppurativa","MeMe","Dermatology","ResearchIsSupport","History","MyLifeMyStory"],"tweet_id":"1325277828951105536","created_at":1618152686,"text":"Pandemic Dreams of Hidradenitis Suppurativa: \n\n#HumanRights #PatientRights #HidradenitisSuppurativa #MeMe #Dermatology #ResearchIsSupport #History #MyLifeMyStory https://t.co/SGvxifwOZy","retweets":6}}},
+{"n":{"type":"node","id":"3663641","labels":["Publication"],"properties":{"updated_at":1617833062,"created_at":1617833062,"publication_date":207705600,"pmid":785606,"sc_publication_id":"3b7a1310faf09ef6c4da6316e3af02ba1845fbbc "}}},
+{"n":{"type":"node","id":"52875064","labels":["Publication"],"properties":{"updated_at":1617885860,"created_at":1617885861,"publication_date":1593561600,"pmid":32602132,"sc_publication_id":"9d44ef027d4b8b406a05c17a24e9dffa353aac49 "}}},
+{"n":{"type":"node","id":"53215103","labels":["Publication"],"properties":{"updated_at":1617886477,"created_at":1617886477,"publication_date":1600300800,"pmid":32942279,"sc_publication_id":"d5d89b2cb7cbcf47e3c0d18853cd80f378f5a7e0"}}},
+{"n":{"type":"node","id":"54042965","labels":["ClinicalTrial"],"properties":{"clinicaltrial_id":"NCT04179175","updated_at":1617944309,"created_at":1617887760,"start_date":"11/01/2020"}}},
+{"n":{"type":"node","id":"54038269","labels":["ClinicalTrial"],"properties":{"clinicaltrial_id":"NCT04414514","updated_at":1617944307,"created_at":1617887758,"start_date":"01/06/2021"}}},
+{"n":{"type":"node","id":"54173439","labels":["ClinicalTrial"],"properties":{"clinicaltrial_id":"NCT01352078","updated_at":1617944367,"created_at":1617887804,"start_date":"01/05/2011"}}},
+{"n":{"type":"node","id":"54725432","labels":["Tweet"],"properties":{"date":1617822030,"sentiment":0.0,"updated_at":1617896785,"favourites":0,"hashtags":["chronicillness"],"tweet_id":"1379871722602631179","created_at":1617896785,"text":"7 Misconceptions About Hidradenitis Suppurativa https://t.co/UTM20K0TCK #chronicillness https://t.co/z3iNXTVyUE","retweets":0}}},
+{"n":{"type":"node","id":"54725428","labels":["Tweet"],"properties":{"date":1617873362,"sentiment":0.0,"updated_at":1617896785,"favourites":0,"hashtags":[],"tweet_id":"1380087026947563523","created_at":1617896785,"text":"Comparing the Effectiveness and Safety Associated With Infliximab vs Infliximab-abda Therapy for Patients With Hidradenitis Suppurativa https://t.co/bjsjzEQjjp","retweets":0}}},
+{"n":{"type":"node","id":"54834514","labels":["Organization"],"properties":{"updated_at":1617904125,"organization_id":"c0f88296-8afb-4a96-9bf4-afdeddc1cdd5","num_cts":396,"name":"Erasmus Medical Center","created_at":1617900891,"num_products":0}}},
+{"n":{"type":"node","id":"54851395","labels":["Organization"],"properties":{"updated_at":1617904128,"organization_id":"5b23f313-b491-4b6b-ad88-fe1848d82b30","num_cts":3,"name":"Microdermis Corporation","created_at":1617900921,"num_products":0}}},
+{"n":{"type":"node","id":"24794784","labels":["Organization"],"properties":{"grid_id":"grid.5254.6","updated_at":1618166220,"organization_id":"450cd56c-064a-4ee6-9644-6b4ccb25678e","num_cts":720,"name":"University of Copenhagen","created_at":1617888851,"category":["Education"],"num_products":0}}},
+{"n":{"type":"node","id":"24796254","labels":["Organization"],"properties":{"updated_at":1617904105,"grid_id":"grid.50550.35","organization_id":"45868c36-73ba-41b5-a4a8-78129dabf533","num_cts":2333,"name":"Assistance Publique – Hôpitaux de Paris","created_at":1617888851,"category":["Healthcare"],"num_products":0}}},
+{"n":{"type":"node","id":"54825746","labels":["Organization"],"properties":{"updated_at":1617904124,"organization_id":"cecd9ff5-6184-4b9d-a368-b18a6add86b7","num_cts":174,"name":"CSL Behring","created_at":1617900875,"num_products":0}}},
+{"n":{"type":"node","id":"1076998","labels":["Author"],"properties":{"sc_author_id":"3773220","h_index":27,"updated_at":1618166141,"name":"Branka  Marinović","created_at":1617832459,"num_publications":89,"num_citations":2390.0}}},
+{"n":{"type":"node","id":"591031","labels":["Author"],"properties":{"sc_author_id":"5256006","h_index":23,"updated_at":1618166141,"name":"Concetta  Potenza","created_at":1617832385,"num_publications":82,"num_citations":1405.0}}},
+{"n":{"type":"node","id":"33931","labels":["Author"],"properties":{"sc_author_id":"5331229","h_index":71,"updated_at":1618165373,"name":"ANDRZEJ  BARTKE","created_at":1617832303,"num_publications":598,"num_citations":20854.0}}},
+{"n":{"type":"node","id":"1166964","labels":["Author"],"properties":{"sc_author_id":"37810403","h_index":1,"updated_at":1618166141,"name":"S D Ali","created_at":1617832471,"num_publications":13,"num_citations":67.0}}},
+{"n":{"type":"node","id":"5719241","labels":["Author"],"properties":{"sc_author_id":"40502703","h_index":3,"updated_at":1618166141,"name":"Elvira  Elena","created_at":1617833503,"num_publications":5,"num_citations":23.0}}},
+{"n":{"type":"node","id":"20822645","labels":["Author"],"properties":{"sc_author_id":"2003547320","h_index":0,"updated_at":1618166141,"name":"M  Oymanns","created_at":1617837553,"num_publications":1,"num_citations":0.0}}},
+{"n":{"type":"node","id":"13131792","labels":["Author"],"properties":{"sc_author_id":"150160157","h_index":1,"updated_at":1618166141,"name":"J-P  Salmin","created_at":1617835234,"num_publications":1,"num_citations":5.0}}},
+{"n":{"type":"node","id":"23430153","labels":["Author"],"properties":{"sc_author_id":"2051949656","h_index":0,"updated_at":1618166141,"name":"Licciardello  Matteo","created_at":1617838356,"num_publications":1,"num_citations":0.0}}},
+{"n":{"type":"node","id":"70077","labels":["Author"],"properties":{"sc_author_id":"1940595","h_index":52,"updated_at":1618165422,"name":"Xue- Qian Zhang","created_at":1617832308,"num_publications":612,"num_citations":13536.0}}},
+{"n":{"type":"node","id":"1921360","labels":["Author"],"properties":{"sc_author_id":"6058106","h_index":11,"updated_at":1618166141,"name":"Zachary Mitchel Bauman","created_at":1617832620,"num_publications":31,"num_citations":267.0}}},
+{"n":{"type":"node","id":"26368813","labels":["Topic"],"properties":{"last_sentiment_score":13.55,"last_trend_prob_var":0.45,"num_pubs":1166,"origin":"E","num_cts":0,"mesh_id":"D000069549","description":"A humanized monoclonal antibody that binds to IL-12 and IL-23 and is used as a DERMATOLOGIC AGENT in the treatment of patients with plaque PSORIASIS who have not responded to other therapies.","created_at":1617840419,"mesh_active":true,"tracked":true,"last_trend_prob":63.99,"uri":"http://id.nlm.nih.gov/mesh/D000069549","mesh_term":"Ustekinumab","updated_at":1617972320,"supplementary_concept":false,"term":"Ustekinumab","topic_id":"45862811-2f62-4ac3-8023-17f3b3aa6219"}}},
+{"n":{"type":"node","id":"75113","labels":["Author"],"properties":{"sc_author_id":"2083608","h_index":60,"updated_at":1618165430,"name":"James Todd Rosenbaum","created_at":1617832308,"num_publications":449,"num_citations":15936.0}}},
+              ]
+
+const ids = nodes.map(n => n.n.id)
+console.log(ids.length, ids)
+
+let relationships = data.map((d, index) => {
+  let {label, start, end} = d.r
+  //return {index, label, startId: start.id, endId: end.id}
+  return {index, label, start, end}
+})
+console.log(relationships[0])
+
+let rels = []
+ids.forEach((id, i) => {
+  let idOthers = ids.filter(i => i!==id)
+  
+  let tempRels = relationships.filter(r => 
+    (r.start.id === id && idOthers.includes(r.end.id)) ||
+    (r.end.id === id && idOthers.includes(r.start.id))
+  )
+  console.log(tempRels.map(l=>l.index))
+  rels = rels.concat(tempRels)
+})
+
+const indexAll = rels.map(l=>l.index).filter((v, i, a) => a.indexOf(v) === i)
+console.log(indexAll.length, indexAll)
+
+
+//sandbox: find interesting nodes
+const candidates = data//.slice(0,10000)
+.filter(d1=>d1.r.label==="worksOn")
+.filter(d2=>d2.r.properties.score>0.95)
+console.log(candidates.map(c=>{
+  const d = c.r
+  return [d.start.labels, d.end.labels]
+}))
+ 
+const relTypes = data//.slice(0, 10)
+.map(d1 => d1.r.label)//{lable: d1.r.label/*, hasProperites: d1.r.properties*/})
+.filter((d2, i, a) => a.indexOf(d2) === i)
+console.log(relTypes)
+/*
+["preferredMappedTo", //topics
+ "isRelatedTo", //tweet, pub -> topic
+ "worksOn", //author, twitterAcc, org -> topic, score
+ "hasSuggestedTopic", //topics
+ "replies", 
+ "retweets", 
+ "tweets", 
+ "mentions", 
+ "quotes", 
+ "cites", 
+ "isAuthor", 
+ "isSponsor", 
+ "hasPublication", 
+ "hasRelatedOrganization", //orgs
+ "hasProduct", 
+ "pharmacologicalAction"]
+ */
+
+
+/*
+- split into 3 categoreis
+- worksOn
+- only 1 connection
+*/
+
+const links = indexAll.map(i => data[i])
+console.log(links)
